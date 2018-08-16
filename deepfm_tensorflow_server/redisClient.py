81219340af44fd127fa0e06d88aab31ce1fbed76
shenxiaoming77 <526633123@qq.com>
2018-08-16 19:57:52 +0800

    'mmmmmmmmmmmmm'

--- deepfm_tensorflow_server/redisClient.py
+++ deepfm_tensorflow_server/redisClient.py
@@ -5,6 +5,12 @@ from  .settings import *
 class RedisClient:
 
     def __init__(self):
-        db = redis.StrictRedis(host = REDIS_HOST,
-                               port = REDIS_PORT,
-                               db = REDIS_DB)
+        self.redis = redis.StrictRedis(host = REDIS_HOST,
+                                       port = REDIS_PORT,
+                                       db = REDIS_DB)
+
+    def get(self, key):
+        return self.redis.get(key)
+
+    def set(self, key, value):
+        return  self.redis.set(key, value)
